65a281a2d7d379d3095ed3e3a134970f032f3255
Ken <napaphon2007@gmail.com>
2026-02-25 20:52:15 +0700

    start

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//Load env vars
+dotenv.config({path:'./config/config.env'});
+
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+
+//route file
+const hospitals = require('./routes/hospitals');
+const appointments = require ('./routes/appointments')
+const auth = require('./routes/auth');
+
+
+
+
+
+//connect to database
+connectDB();
+
+const app=express();
+
+app.set('query parser','extended')
+
+//Body parser
+app.use(express.json());
+
+//Cookie parser
+app.use (cookieParser());
+
+
+app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/appointments',appointments);
+app.use('/api/v1/auth',auth);
+
+
+
+const PORT=process.env.PORT || 5000;
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+});
