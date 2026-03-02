8cb0e3b5db87dcc59260c7f0c3e692f9e38ca307
Ken <napaphon2007@gmail.com>
2026-03-02 21:24:06 +0700

    add update name and tel function

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 // routes/auth.js
 const express= require('express');
-const {register,login,getMe,logout}=require('../controllers/auth');
+const {register,login,getMe,logout,updateMe}=require('../controllers/auth');
 
 const router=express.Router();
 
@@ -11,5 +11,7 @@ router.post('/register',register);
 router.post('/login',login);
 router.get('/me',protect,getMe);
 router.get('/logout',protect, logout);
+router.put('/updateme', protect, updateMe);
+
 
 module.exports=router;
